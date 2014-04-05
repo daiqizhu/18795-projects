d771f550110089fead534d8b88fed24eb2a97db0
yoowonsun <halalujah305@gmail.com>
2014-04-05 09:25:11 -0400

    Just a placeholder file. I made this on my destop, and want to work on my laptop later today so I pushed this (it's basically empty)
    -Alex Yoo

---
+++ Project 3/src/computeUniformIllumination.m
@@ -0,0 +1,19 @@
+function [ uniformity ] = computeUniformIllumination( image, noise_min, noise_max )
+%
+% Used for Part B.3
+%
+% displayNoiseHistogram: calculates the uniformity of illumination
+%
+% Inputs:  image - a normal 2D image to calculate uniformity for
+%          noise_min - minimum value of a segment of noisy background
+%          noise_max - maximum value of a segment of noisy background
+%
+% Outputs: a scalar representing the uniformity of illuminatoin of image
+%
+
+% Filter with a large low-pass kernel
+
+
+
+end
+
