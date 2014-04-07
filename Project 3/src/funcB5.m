8252260bae4f72206d26b7d5201983b9f2e26bb0
ozaniskilibli <ozaniskilibli@gmail.com>
2014-04-07 15:08:00 -0400

    Part B5, created a place holder function
    I have created a function named funcB5 for the part B5. Its output is
    incorrect. I suspect that the kernel size is problematic. I'm running
    short on statistics knowledge here. Someone please jump in.

---
+++ Project 3/src/funcB5.m
@@ -0,0 +1,40 @@
+function funcB5
+
+clear all
+close all
+
+I = im2double(imread('../images/image01.tiff'));
+
+sigmaU = 10;
+sigmaV = 5;
+theta = [0 30 60 90 120 150];
+
+for i = 1:length(theta)
+    kernel{i} = anisoGaussian(sigmaU, sigmaV, theta(i));
+    y = filter2(kernel{i}, I);
+    figure;imshow(y,[])
+end
+
+
+function kernel = anisoGaussian(sigmaU, sigmaV, theta)
+%This part, I have asked professor
+phi = atand((sigmaV^2*cosd(theta)^2 + sigmaU^2*sind(theta)^2)/...
+    ((sigmaU^2-sigmaV^2)*sind(theta)*cosd(theta)));
+sigmaX = sigmaU*sigmaV /sqrt( sigmaV^2*cosd(theta)^2 + ...
+    sigmaU^2*sind(theta)^2 );
+sigmaP = 1/sind(phi) *...
+    sqrt( sigmaV^2*cosd(theta)^2 + sigmaU^2*sind(theta)^2 );
+
+% This part I am not sure (probably wrong)
+x = -round(sigmaU):1:round(sigmaU);
+y = -round(sigmaP):1:round(sigmaP);
+kernel = zeros(length(x),length(y));
+for i = 1:length(x)
+    for j = 1:length(y)
+        kernel(i,j) = 1/(2*pi*sigmaX*sigmaP)*exp(-0.5*( ...
+            (x(i)-y(j)/tand(phi))^2/sigmaX^2 + ...
+            (y(j)/sind(phi))^2/sigmaP^2 ) );
+    end
+end
+
+
