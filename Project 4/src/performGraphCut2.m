d1bb3bc5805c359c2214f2124f5b61c65148d312
yoowonsun <halalujah305@gmail.com>
2014-05-04 22:58:34 -0400

    Removed unnecessary algorithms, and fixed directory stuff in performGraphCut2.m
    -Alex Yoo

--- Project 4/src/performGraphCut2.m
+++ Project 4/src/performGraphCut2.m
@@ -14,17 +14,17 @@ addpath('graph_cut_based_algs/algorithm2/');
 
 % Two static images
 main_CV('graph_cut_based_algs/static_images/60x_02.tif',...
-    'graph_cut_based_algs/algorithm2/results/60x_02.tif');
+    '../results/Part C.1.1/60x_02.tif');
 pause
 main_CV('graph_cut_based_algs/static_images/Blue0001.tif',...
-    'graph_cut_based_algs/algorithm2/results/Blue0001.tif');
+    '../results/Part C.1.1/Blue0001.tif');
 pause
 
 % Sequence of images
 % Iterate through each image
 for i = 2001:2150
     filename = sprintf('graph_cut_based_algs/sequence_images/MitoGFP_LgtGal4_a01r01s0%d.tif',i);
-    outputname = sprintf('graph_cut_based_algs/algorithm2/results/sequence/MitoGFP_LgtGal4_a01r01s0%d.tif',i);
+    outputname = sprintf('../results/Part C.1.1/sequence/MitoGFP_LgtGal4_a01r01s0%d.tif',i);
     main_CV(filename,outputname);
 end
 % The AVI creation was done in ImageJ
